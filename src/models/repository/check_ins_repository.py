1f5a8754ae4b2b3f28f6a9c6b056d8bce3d930b6
renato-avellar <o.onizuka@icloud.com>
2024-04-02 23:19:52 -0300

    feat: Implementing CheckIn_repository

---
+++ src/models/repository/check_ins_repository.py
@@ -0,0 +1,22 @@
+from src.models.settings.connection import db_connection_handler
+from src.models.entitities.check_ins import CheckIns
+from sqlalchemy.exc import IntegrityError
+
+class CheckInRepository:
+    def insert_check_in(self, attendee_id: str) -> str:
+        with db_connection_handler as database:
+           try:
+                check_in = CheckIns(attendeeId = attendee_id)
+            
+                database.session.add(check_in)
+                database.session.commit()
+            
+                return check_in
+            
+           except IntegrityError:
+               raise Exception('Check in já cadastrado')
+           
+           except Exception as exception:
+               database.session.rollback()
+               raise exception
+           
