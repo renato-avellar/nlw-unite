4ca138a6d455f24d54ffc5accef11dd173abdc2e
renato-avellar <o.onizuka@icloud.com>
2024-04-04 22:34:13 -0300

    feat: Implementing check_in routes

---
+++ src/data/check_in_handler.py
@@ -0,0 +1,13 @@
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
+from src.models.repository.check_ins_repository import CheckInRepository
+
+class CheckInHandler:
+    def __init__(self) -> None:
+        self.__check_in_repository = CheckInRepository()
+        
+    def registry(self, http_request: HttpRequest) -> HttpResponse:
+        check_in_infos = http_request.param["attendee_id"]
+        self.__check_in_repository.insert_check_in(check_in_infos)
+        
+        return HttpResponse(body=None, status_code=201)
